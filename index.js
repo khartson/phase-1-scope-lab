2119112d359422c49c02e95a3cae399102a6903a
Kyle <khartson2017@gmail.com>
2021-10-17 14:38:11 -0400

    commit tree 45d70643d4b3c2fc633885f64aeade7f0bf00e58

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'; 
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'; 
+  // return bestCusomer;
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'; 
+}
+const leastFavoriteCustomer = "Kyle"
+
+function changeLeastFavoriteCustomer() { 
+  leastFavoriteCustomer = "not Kyle"; 
+}
